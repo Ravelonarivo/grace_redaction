6b874daa9f6158ac75d618e25374ce513a54e778
ravelonarivo <irinaravelonarivo@gmail.com>
2021-03-29 11:06:51 +0000

    presentation component created

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import Image from 'next/image';
-import Link from 'next/link'; 
 import Layout from '../components/layout';
+import Presentation from '../components/presentation/presentation';
 
 const Home = () => {
   return (
@@ -12,29 +10,11 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-        <section id="home">
-          <div className={ styles.bgWrap }>
-            <Image
-              src="/background_7.jpg"
-              layout="fill"
-              objectFit="cover"
-              quality={100}
-              alt="background-image"
-            />
-          </div>
-          <div className={ styles.bgTextAndButton }>
-            <p className={ styles.bgText + ' dark-blue fw8' }>
-              Confiez nous vos projets.
-            </p>
-            <div class="tc ph3">
-              <a class="f4 grow no-underline br-pill ph3 pv2 mb2 dib white bg-dark-blue" href="#0">Voir nos offres</a>
-            </div>
-          </div>
-        </section>
-
-        <section id="offers">
-          <p>Test</p>
-        </section>
+      <Presentation />
+      
+      <section id="offers">
+        <p>Test</p>
+      </section>
     
     </Layout>
   );
