fb3885f028f90df0ca15a696809348e72f7b22c1
ravelonarivo <irinaravelonarivo@gmail.com>
2021-03-29 10:33:24 +0000

    layout component created

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,34 @@
 import styles from './layout.module.css';
+import Image from 'next/image';
 
 const Layout = ({ children }) => {
-	return <div className={ styles.container }>{ children }</div>
+	return (
+		<>
+			<nav className={ styles.sticky + ' db dt-l w-100 border-box pa3 ph6-l bg-white shadow-3' }>
+		        <a className="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" href="#" title="Home">
+		          <Image
+		            src="/logos/logo_2.png"
+		            height={60}
+		            width={60}
+		            alt="logo"
+		          />
+		        </a>
+		        <div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
+		          <a className="link dim f6 f5-l dib dark-blue b mr3 mr4-l" href="#home" title="Accueil">Accueil</a>
+		          <a className="link dim f6 f5-l dib dark-blue b mr3 mr4-l" href="#offers" title="Offres">Offres</a>
+		          <a className="link dim f6 f5-l dib dark-blue b mr3 mr4-l" href="#" title="Processus">Processus</a>
+		          <a className="link dim f6 f5-l dib dark-blue b mr3 mr4-l" href="#" title="Contact">Contact</a>
+		          <a className="link dim f6 f5-l dib dark-blue b " href="#" title="Blog">Blog</a>
+		        </div>
+		      </nav>
+		      
+		      <main>{children}</main>
+		      
+		      <footer>  
+		        footer 
+		      </footer>
+		</>
+	);
 };
 
 export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -2,35 +2,17 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Image from 'next/image';
 import Link from 'next/link'; 
+import Layout from '../components/layout';
 
 const Home = () => {
   return (
-    <div>
+    <Layout>
       <Head>
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <nav className={ styles.sticky + ' db dt-l w-100 border-box pa3 ph6-l bg-white shadow-3' }>
-        <a className="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" href="#" title="Home">
-          <Image
-            src="/logos/logo_2.png"
-            height={60}
-            width={60}
-            alt="logo"
-          />
-        </a>
-        <div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
-          <a className="link dim f6 f5-l dib dark-blue b mr3 mr4-l" href="#" title="Accueil">Accueil</a>
-          <a className="link dim f6 f5-l dib dark-blue b mr3 mr4-l" href="#" title="Offres">Offres</a>
-          <a className="link dim f6 f5-l dib dark-blue b mr3 mr4-l" href="#" title="Processus">Processus</a>
-          <a className="link dim f6 f5-l dib dark-blue b mr3 mr4-l" href="#" title="Contact">Contact</a>
-          <a className="link dim f6 f5-l dib dark-blue b " href="#" title="Blog">Blog</a>
-        </div>
-      </nav>
-
-      <main> 
-        <section>
+        <section id="home">
           <div className={ styles.bgWrap }>
             <Image
               src="/background_7.jpg"
@@ -50,14 +32,11 @@ const Home = () => {
           </div>
         </section>
 
-        <section>
+        <section id="offers">
           <p>Test</p>
         </section>
-      </main>
-
-      <footer>   
-      </footer>
-    </div>
+    
+    </Layout>
   );
 };
 
