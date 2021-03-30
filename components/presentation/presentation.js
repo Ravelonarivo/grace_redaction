497a15f9eb9948607b13473b7d245ce7f1e68168
ravelonarivo <irinaravelonarivo@gmail.com>
2021-03-30 18:44:23 +0000

    edit text

--- components/presentation/presentation.js
+++ components/presentation/presentation.js
@@ -16,7 +16,7 @@ const Presentation = () => {
 	        </div>
 	        <div className={ styles.bgTextAndButton }>
 	        	<p className={ styles.bgText + ' dark-blue fw8' }>
-	              Confiez nous vos projets.
+	              Confiez-nous vos projets.
 	            </p>
 	            <div class="tc ph3">
 	              <a class="f4 grow no-underline br-pill ph3 pv2 mb2 dib white bg-dark-blue" href="#0">Voir nos offres</a>
