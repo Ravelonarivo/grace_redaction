6f4c8c01165288f539e7e01b375b32a3dabb00ff
ravelonarivo <irinaravelonarivo@gmail.com>
2021-03-31 13:57:59 +0000

    footer done

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,10 @@
 import styles from './layout.module.css';
 import Image from 'next/image';
+import { MailOutline } from 'react-ionicons';
 
 const Layout = ({ children }) => {
+	const date = new Date();
+
 	return (
 		<>
 			<nav className={ styles.sticky + ' db dt-l w-100 border-box pa3 ph7-l bg-white shadow-3' }>
@@ -10,7 +13,7 @@ const Layout = ({ children }) => {
 			            src="/logos/logo_2.png"
 			            height={60}
 			            width={60}
-			            alt="logo"
+			            alt="logo-nav"
 			        />
 		        </a>
 		        <div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
@@ -24,9 +27,67 @@ const Layout = ({ children }) => {
 		      
 		    <main className>{children}</main>
 		      
-		    <footer>  
-		    	footer 
-		    </footer>
+		    <footer class="bg-near-black white-80 pv5 ph7 vh-75">
+		    	<div className="h-100">
+			    	<div className="fl w-25 h-100">
+			    		<Image
+			    			src="/logos/logo_5.png"
+			    			height={60}
+			    			width={60}
+			    			alt="logo-footer"
+			    		/>
+			    		<a className="link dim db f5 mb2 mt3" href="#">Accueil</a>
+			    		<a className="link dim db f5 mb2" href="#">Offres</a>
+			    		<a className="link dim db f5 mb2" href="#">Processus</a>
+			    		<a className="link dim db f5 mb2" href="#">Blog</a>
+			    		<a className="link dim db f5 mb2" href="#">Contact</a>
+			    	</div>
+			    	<div className="fl w-25 h-100">
+			    		<h2 className="f2 fw4 ma0">Nos Offres</h2>
+			    		<a className="link dim db f5 mb2 mt3" href="#">Rédaction</a>
+			    		<a className="link dim db f5 mb2" href="#">Réécriture</a>
+			    		<a className="link dim db f5 mb2" href="#">Correction</a>
+			    	</div>
+			    	<div className="fl w-25 h-100">
+			    		<div className="flex">
+				    		<MailOutline
+							  	color={'#fffff'} 
+							  	title={'email'}
+							  	height="1.5rem"
+							  	width="1.5rem"
+							/>
+							<a className="link dim f5 ml2" href="#">contact@graceredaction.com</a>
+						</div>
+			    	</div>
+			    	<div className="tc fr w-25 h-100">
+			    		<a className="link dim mr3" href="#">
+			    			<Image
+								src="/facebook.svg"
+								alt="icon-facebook"
+								width={30}
+								height={30}
+							/>
+			    		</a>
+			    		<a className="link dim mr3" href="#">
+			    			<Image
+								src="/instagram.svg"
+								alt="icon-instagram"
+								width={30}
+								height={30}
+							/>
+			    		</a>
+			    		<a className="link dim" href="#">
+			    			<Image
+								src="/twitter.svg"
+								alt="icon-twitter"
+								width={30}
+								height={30}
+							/>
+			    		</a>
+			    	</div>
+		    	</div>
+				<p class="f5 tc">© { date.getFullYear() } Grace Rédaction.</p>
+			</footer>
 		</>
 	);
 };
