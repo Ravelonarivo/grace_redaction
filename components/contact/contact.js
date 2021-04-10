6a4a85b3d7fad7abaf19b7221931ca4fd9ee03b7
ravelonarivo <irinaravelonarivo@gmail.com>
2021-04-10 23:01:26 +0000

    update contact design

--- components/contact/contact.js
+++ components/contact/contact.js
@@ -14,11 +14,14 @@ const Contact = ({ contactSectionRef }) => {
 				</defs>
 				<path d="M 0,400 C 0,400 0,200 0,200 C 52.041605301914586,165.5 104.08321060382917,131 164,147 C 223.91678939617083,163 291.7087628865979,229.5 347,260 C 402.2912371134021,290.5 445.0817378497791,285 513,271 C 580.9182621502209,257 673.9642857142858,234.5 732,231 C 790.0357142857142,227.5 813.0611192930779,243.00000000000003 871,248 C 928.9388807069221,252.99999999999997 1021.7912371134023,247.5 1083,232 C 1144.2087628865977,216.5 1173.7739322533137,191 1228,184 C 1282.2260677466863,177 1361.1130338733433,188.5 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" stroke-width="0" fill="url(#gradient)" className="transition-all duration-300 ease-in-out delay-150"></path>
 			</svg>
-			<div ref={ contactSectionRef } id="contact" className={ styles.background }>
-				<h2 className={ styles.title + ' tc f2 white fw5 mt0' }>Entrez en contact avec nous</h2>
-				<div className="vh-75">
-					<div className="mh7 h-100">
-						<div className="fl h-100 w-60 pv4 ph4">
+			<div className="vh-100 w-100">
+				<video className={ styles.video + ' absolute w-100 h-100' } autoPlay muted loop>
+					<source src="/contact.mp4" type="video/mp4"/> 
+				</video>
+				<div ref={ contactSectionRef } id="contact" className={ styles.background + ' absolute o-90 h-100 w-100 pt4' }>
+					<h2 className={ styles.title + ' tc f2 white fw5 mt0' }>Entrez en contact avec nous</h2>
+					<div className="mh7">
+						<div className="fl w-60 pv4 ph4">
 							<h3 className={ styles.color + ' tc f4 fw5' }>En nous écrivant</h3>
 							<form className="mt4">
 								<label htmlFor="contact-form-name" className="f5 white fw5">Nom</label>
@@ -27,10 +30,10 @@ const Contact = ({ contactSectionRef }) => {
 								<input id="contact-form-email" className={ styles.formular + ' db ba pa2 w-100 h2 mt2 mb4 b--black-20 br2' } type="email" placeholder="Entrez votre adresse email" required/>
 								<label  htmlFor="contact-form-message" className="f5 white fw5">Message</label>
 								<textarea id="contact-form-message" className={ styles.formular + ' db pa2 w-100 h4 mt2 mb4 b--black-20 br2' } placeholder="Entrez votre message" required/>
-								<input className={ styles.submitButton + ' pointer br-pill grow f5 white fw5 h2 ph4 b--dark-blue ba pb2' } type="submit" value="Envoyer"/>
+								<input className={ styles.submitButton + ' pointer br-pill grow f5 white fw5 h2 ph4 ba pb2' } type="submit" value="Envoyer"/>
 							</form>
 						</div>
-						<div className={ styles.subTitle + " fr h-100 w-40 pv4 ph4 br--top br4" }>
+						<div className={ styles.subTitle + " fr w-40 pv4 ph4 br4" }>
 							<h3 className="f4 fw5 mb4">Ou via les réseaux sociaux</h3>
 							<a className="db link dim mb4 f5" href="#">
 								<Image
@@ -60,7 +63,7 @@ const Contact = ({ contactSectionRef }) => {
 								<b className="db mt3 mb2">twitter</b>@graceredaction				
 							</a>
 						</div>
-					</div>
+					</div>						
 				</div>
 			</div>
 		</>
